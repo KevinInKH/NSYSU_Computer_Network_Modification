0d18937a13d904d8cd0073c7f882819af5b0aab7
KevinInKH <90780726+KevinInKH@users.noreply.github.com>
2024-01-12 10:11:48 +0800

    2024

--- client_test.cpp
+++ client_test.cpp
@@ -1,36 +1,43 @@
-#include <string.h>
-#include <time.h>
+#include "client_class.hpp"
+
 // for multi thread
 #include <pthread.h>
-#include "client_class.hpp"
+
 typedef void * (*thread_ptr)(void *);
 
 using namespace std;
 
-// poisson distribution
-// std::default_random_engine generator;
-// std::poisson_distribution<int> distribution(4.1);
-
 // test for client
 int main(int argc, char *argv[]) {
 
-	srand(time(NULL));
+	vector<string> requests;
+	char *hostname = (char *) malloc(20);
+	strcpy(hostname, argv[1]);
+	int port = atoi(argv[2]);
+
+	for(int i=3;i<argc;i++){
+		requests.push_back(argv[i]);
+	}
 
 	int client_num = 1;
-	string testbench[6]={"5 + 2", "5 - 2", "5 * 2", "5 / 2", "5 ** 2", "6 ** 2"};
-	string testbench_video[7] = {"pic/Lenna.jpg", "vid/1.mp4", "vid/2.mp4", "vid/3.mp4", "vid/4.mp4", "vid/5.mp4", "vid/6.mp4"};
 
 	client_class* myClient[client_num];
 
-	for(int i=0;i<client_num;i++)	(myClient[i]) = new client_class();
+	for(int i=0;i<client_num;i++){
+		(myClient[i]) = new client_class(hostname, port);
+	}
+
 	pthread_t request_thread[100];
 	cout << "client build complete\n";
 
 	// start sending message
 	for(int i=0;i<client_num;i++){
+
 		// set the sending packet
-		// myClient[i]->set_pkt(testbench[(rand()%6)]);
-		myClient[i]->set_request(testbench_video[1]);
+		for(int p=0;p<requests.size();p++){
+			myClient[i]->set_request(requests[p]);
+		}
+
 		if(pthread_create(&request_thread[i],
 							NULL,
 							(thread_ptr)&client_class::processing, 	// pointer to member function
