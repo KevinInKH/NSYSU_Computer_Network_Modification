ba74b77d98f18e4c818135beb232a1401d643658
KevinInKH <90780726+KevinInKH@users.noreply.github.com>
2024-01-12 10:13:07 +0800

    2024

--- server_test.cpp
+++ server_test.cpp
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-// for poisson distribution
-#include <random>
-// for multi thread
-#include <pthread.h>
 #include "server_class.hpp"
 
 using namespace std;
